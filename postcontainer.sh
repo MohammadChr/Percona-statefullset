cabdc82c1ec6be3b53ecad3334830645fc106f2a
Mohammad Cheraghchi <mohammad.ch@payrad.org>
2023-03-12 22:37:25 +0330

    etid

--- postcontainer.sh
+++ postcontainer.sh
@@ -2,9 +2,10 @@ sleep 20
 server_id=$(echo $HOSTNAME | cut -d- -f2) && echo $server_id
 if [[ server_id -eq 0 ]]; then
     mysql -uroot -proot -e "CREATE USER 'replica_user'@'%' IDENTIFIED WITH mysql_native_password BY 'password';"
-    mysql -uroot -proot -e "GRANT REPLICATION SLACE ON *.* TO 'replica_user'@'%';"
-    mysql -uroot -proot -e "flush privileges"
+    mysql -uroot -proot -e "GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';"
+    mysql -uroot -proot -e "FLUSH PRIVILEGES"
 else
     mysql -uroot -proot -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='mystatefulset-0.percona', SOURCE_USER='replica_user', SOURCE_PASSWORD='password';"
 fi
+
 
