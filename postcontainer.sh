220bcd2b9f39fef380efe7f43084ae6b1972779f
Mohammad cheraghchi <110486772+MohammadChr@users.noreply.github.com>
2023-03-12 22:29:40 +0330

    Update postcontainer.sh

--- postcontainer.sh
+++ postcontainer.sh
@@ -3,7 +3,7 @@ server_id=$(echo $HOSTNAME | cut -d- -f2) && echo $server_id
 if [[ server_id -eq 0 ]]; then
     mysql -uroot -proot -e "CREATE USER 'replica_user'@'%' IDENTIFIED WITH mysql_native_password BY 'password';"
     mysql -uroot -proot -e "GRANT REPLICATION SLACE ON *.* TO 'replica_user'@'%';"
-    mysql -uroot -proot -e "flush privileges"
+    mysql -uroot -proot -e "FLUSH PRIVILEGES"
 else
     mysql -uroot -proot -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='mystatefulset-0.percona', SOURCE_USER='replica_user', SOURCE_PASSWORD='password';"
 fi
