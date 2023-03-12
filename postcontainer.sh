b5eea0c3b1af8f76e02a3306baac68af695c7a8a
Mohammad Cheraghchi <mohammad.ch@payrad.org>
2023-03-12 21:25:54 +0330

    .

--- postcontainer.sh
+++ postcontainer.sh
@@ -5,6 +5,6 @@ if [[ server_id -eq 0 ]]; then
     mysql -uroot -proot -e "GRANT REPLICATION SLACE ON *.* TO 'replica_user'@'%';"
     mysql -uroot -proot -e "flush privileges"
 else
-    mysql -uroot -proot -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='mysql-0.mysql', SOURCE_USER='replica_user', SOURCE_PASSWORD='password';"
+    mysql -uroot -proot -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='mystatefulset-0.percona', SOURCE_USER='replica_user', SOURCE_PASSWORD='password';"
 fir
 
